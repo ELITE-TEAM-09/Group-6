7d0e560b80453ddb9400c0eeaf79f91db2a6dd9d
Fatuma Ibrahim <fatmazahraabdulrashid@gmail.com>
2024-03-27 17:34:35 +0300

    complete

---
+++ README.md
@@ -0,0 +1,2 @@
+## Combine Arrays:
+Write a function that takes two arrays and returns a new array that combines both arrays.
---
+++ idex.js
@@ -0,0 +1,12 @@
+
+function combineArrays(array1, array2) {
+    return array1.concat(array2);
+}
+
+
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+//combining the two arrays 
+let combined = combineArrays(array1, array2);
+console.log(combined);
+ 
